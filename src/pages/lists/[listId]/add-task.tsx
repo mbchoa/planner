e7723b76dcb5c779b6ec4513661644303542a301
Michael-Bryant Choa <mbchoa@gmail.com>
2023-04-09 02:17:37 -0700

    add /lists/:listId/add-task page

--- src/pages/lists/[listId]/add-task.tsx
+++ src/pages/lists/[listId]/add-task.tsx
@@ -6,12 +6,12 @@ import { twMerge } from "tailwind-merge";
 import { z } from "zod";
 
 const FormValueSchema = z.object({
-  listName: z.string().min(1, { message: "List name is required" }),
+  description: z.string().min(1, { message: "Task description is required" }),
 });
 
 type IFormValues = z.infer<typeof FormValueSchema>;
 
-export default function AddListPage() {
+export default function AddTaskPage() {
   const {
     register,
     handleSubmit,
@@ -20,25 +20,26 @@ export default function AddListPage() {
     resolver: zodResolver(FormValueSchema),
   });
 
-  const onSubmit: SubmitHandler<IFormValues> = (data) => alert(data.listName);
+  const onSubmit: SubmitHandler<IFormValues> = (data) =>
+    alert(data.description);
 
-  const hasErrors = errors.listName !== undefined;
+  const hasErrors = errors.description !== undefined;
 
   return (
     <main className="flex h-screen flex-col items-center justify-center p-4">
       <form onSubmit={handleSubmit(onSubmit)}>
         <label
-          htmlFor="listName"
+          htmlFor="description"
           className={twMerge(
             "block text-sm font-medium leading-6 text-gray-900",
             hasErrors && "text-red-600"
           )}
         >
-          List Name
+          Description
         </label>
         <div className="relative mt-2 rounded-md shadow-sm">
           <input
-            {...register("listName")}
+            {...register("description")}
             type="text"
             className={twMerge(
               "block w-full rounded-md border-0 py-1.5 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-300 focus:ring-2 focus:ring-inset focus:ring-gray-500 sm:text-sm sm:leading-6",
@@ -58,7 +59,7 @@ export default function AddListPage() {
         </div>
         <ErrorMessage
           errors={errors}
-          name="listName"
+          name="description"
           className="mt-2 text-sm text-red-600"
           as="span"
         />
@@ -67,7 +68,7 @@ export default function AddListPage() {
             className="hover:bg-grau-500 focus-visible:outline-grau-600 w-full rounded-md bg-gray-600 px-2.5 py-2.5 font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
             type="submit"
           >
-            Create
+            Add Task
           </button>
         </div>
       </form>
